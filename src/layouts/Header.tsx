328914d18c7dfe03f767e5d0299b8d442b533bb0
IceHugh <ygz14835187@163.com>
2025-05-23 08:31:58 +0800

    feat(ui): :sparkles: add beta

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -3,7 +3,8 @@ import { ThemeSwitcher } from "@/components/common/ThemeSwitcher";
 import { ConnectButton } from "@/components/wallet/ConnectButton";
 export default function Header() {
   return <nav className="h-14 sticky top-0 z-50 bg-background/80 backdrop-blur-sm px-4">
-    <div className="h-full container mx-auto flex justify-end items-center gap-2">
+    <div className="h-full container mx-auto flex justify-between items-center gap-2">
+      <span className="inline-block rounded-full bg-yellow-200 text-yellow-800 text-xs font-bold px-3 py-1 mr-2 shadow-sm select-none">Beta</span>
       <div className="flex items-center gap-2">
         <ConnectButton />
         <SuiNetworkSwitcher />
